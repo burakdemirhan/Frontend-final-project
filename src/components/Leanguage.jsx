59ad329bef5fc0b25a839867aa02ed1c2afff60b
burakdemirhan <burkidad@outlook.com>
2024-11-06 19:48:55 +0300

    dil degisimi icin olusturulan butonun yanlıs sekansta oldugu fark edildi düzeltildi

--- src/components/Leanguage.jsx
+++ src/components/Leanguage.jsx
@@ -11,11 +11,11 @@ export default function Leanguage() {
       <button onClick={handleClick} className="text-[#777777]">
         {leanguage === "tr" ? (
           <>
-            <span className="text-[#4731D3]">Türkçe</span>'YE GEÇ
+            <span className="text-[#4731D3]">Switch</span> TO ENGLISH
           </>
         ) : (
           <>
-            <span className="text-[#4731D3]">Switch</span> TO ENGLISH
+            <span className="text-[#4731D3]">TÜRKÇEYE</span>'YE GEÇ
           </>
         )}
       </button>
